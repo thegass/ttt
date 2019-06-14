45f53e22a4a01eef70ec7d1fab67862ac48650f2
Oliver Gassner <ogassner@outlook.at>
2019-06-14 23:11:39 +0200

    [FEATURE] player vs player complete

--- Board.py
+++ Board.py
@@ -28,8 +28,12 @@ class Board():
 
     def __init__(self):
         self.state = {
-            'X' : 0b000000000,
-            'O' : 0b000000000
+            'X': 0b000000000,
+            'O': 0b000000000
+        }
+        self.rounds = {
+            'X': 0,
+            'O': 0
         }
         self.starter = self.getStarter()
         self.winner = False
@@ -39,7 +43,7 @@ class Board():
         return ('X', 'O')[random.randint(0, 1)]
 
     def switchPlayer(self):
-        self.player=('X','O')[self.player=='X']
+        self.player = ('X', 'O')[self.player == 'X']
 
     def getEmptyCells(self):
         return ~(self.state['X'] | self.state['O']) & ((1 << 9)-1)
@@ -66,20 +70,50 @@ class Board():
                 print(divider)
                 line = ''
 
-    
+    def move(self):
+        move = input('Move for '+game.player+': ')
+        moveMask = self.transformCoordinates(move)
+        if (moveMask == False):
+            self.move()
+        else:
+            if (self.isMovePossible(moveMask)):
+                self.state[self.player] |= moveMask
+            else:
+                print('move not possible')
+                self.move()
+
+    def isMovePossible(self, moveMask):
+        empty = self.getEmptyCells()
+        return moveMask & empty
+
+    def playGame(self):
+        self.print()
+        while self.winner == False:
+            self.rounds[self.player] += 1
+            self.move()
+            self.print()
+            self.checkForWinner()
+            self.switchPlayer()
+
+    def checkForWinner(self):
+        print('check for win')
+        for possibleWin in self.winners:
+            if ((possibleWin & self.state[self.player]) == possibleWin):
+                print("Winner is player "+self.player+", needed " +
+                      str(self.rounds[self.player])+" rounds to win")
+                self.winner = self.player
 
     def transformCoordinates(self, move):
         """transform x,y to binary"""
-        return self.moves[move]
+        if move in self.moves.keys():
+            return self.moves[move]
+        else:
+            print('move out of range')
+            return False
 
 
 if __name__ == '__main__':
     game = Board()
-    while game.winner==False:
-        move= input('Move for '+game.player+': ')
-        game.state[game.player] |= game.transformCoordinates(move)
-        game.print()
-        game.switchPlayer()
-
+    game.playGame()
 
     # print(bin(game.getEmptyCells())[1])
