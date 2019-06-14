3bc1276010c4e14bf8aac0ec80195d5dc6d523f7
Oliver Gassner <ogassner@outlook.at>
2019-06-14 22:38:13 +0200

    [FEATURE] print board, read & execute moves

--- Board.py
+++ Board.py
@@ -1,10 +1,12 @@
-class Board():
-    def __init__(self):
-        self.stateX = 0b000000000
-        self.stateO = 0b000000000
+import random
 
-    def setPiece(self, player, position):
-        """update state"""
+
+class Board():
+    pieces = {
+        'X': ' X ',
+        'O': ' O ',
+        '.': '   '
+    }
 
     moves = {
         '1,1': 0b100000000,
@@ -24,6 +26,60 @@ class Board():
         0b100010001, 0b001010100
     }
 
+    def __init__(self):
+        self.state = {
+            'X' : 0b000000000,
+            'O' : 0b000000000
+        }
+        self.starter = self.getStarter()
+        self.winner = False
+        self.player = self.starter
+
+    def getStarter(self):
+        return ('X', 'O')[random.randint(0, 1)]
+
+    def switchPlayer(self):
+        self.player=('X','O')[self.player=='X']
+
+    def getEmptyCells(self):
+        return ~(self.state['X'] | self.state['O']) & ((1 << 9)-1)
+
+    def print(self):
+        # display board
+        divider = '+---+---+---+'
+        print(divider)
+        count = 0
+        line = ''
+        for value in self.moves.values():
+            line += '|'
+            if (value & self.state['O']):
+                line += self.pieces['O']
+            if (value & self.state['X']):
+                line += self.pieces['X']
+            if (value & self.getEmptyCells()):
+                line += self.pieces['.']
+            count += 1
+            if (count == 3):
+                line += '|'
+                print(line)
+                count = 0
+                print(divider)
+                line = ''
+
+    
+
     def transformCoordinates(self, move):
         """transform x,y to binary"""
         return self.moves[move]
+
+
+if __name__ == '__main__':
+    game = Board()
+    while game.winner==False:
+        move= input('Move for '+game.player+': ')
+        game.state[game.player] |= game.transformCoordinates(move)
+        game.print()
+        game.switchPlayer()
+
+
+    # print(bin(game.getEmptyCells())[1])
